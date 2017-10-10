af85516ad442ba6809a4686ea4395d619d50cbb2
yuki540 <apple.yamamoto@ezweb.ne.jp>
2017-10-10 23:03:48 +0900

    add: ロード画面の実装

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,14 @@
-console.log('Hello, yuki540.')
+resizeLoadView()
+
+/* resize -------------------------------------------------------------------- */
+window.addEventListener('resize', () => {
+  resizeLoadView()
+})
+
+function resizeLoadView() {  
+  const load_view = document.querySelectorAll('.load-view div')
+  for(let i=0; i < load_view.length; i++) {
+    load_view[i].style.width  = `${ document.body.offsetWidth }px`
+    load_view[i].style.height = `${ window.innerHeight }px`
+  }
+}
