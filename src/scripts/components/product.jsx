57c51b90788317a131eb968019668d998c91df30
yuki540 <tabletennis540@gmail.com>
2017-12-06 09:55:35 +0900

    fix: デザイン項目の記述忘れを修正

--- src/scripts/components/product.jsx
+++ src/scripts/components/product.jsx
@@ -36,6 +36,19 @@ export default class Product extends React.Component {
             )
           })}
           </div>
+          <h2><span className="fa fa-file-code-o"></span>デザイン</h2>
+          <div>
+          {product.design.map((li, key) => {
+            return (
+              <a href={ li.link } target="_blank" className="product-li" key={ key }>
+                <img src={ li.thumb } alt="サムネ" />
+                <h3>{ li.name }</h3>
+                <p>{ li.description }</p>
+                <small>{ li.date }</small>
+              </a>
+            )
+          })}
+          </div>
           <h2><span className="fa fa-film"></span>動画</h2>
           <div>
           {product.movie.map((li, key) => {
